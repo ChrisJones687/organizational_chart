1b9f72704d825929bec52af27fd63e88f9118f10
Chris Jones <cjones1688@gmail.com>
2018-06-12 16:21:42 -0400

    setup org chart and aphis projects

---
+++ aphis_project.R
@@ -0,0 +1,61 @@
+library(DiagrammeR)
+
+grViz("
+      
+      digraph boxes_and_circles {
+      
+      # add node statements
+      node [shape = box
+      fontname = Arial
+      perwidth = 4.0
+      style = filled
+      fontsize = 16
+      fillcolor = CornflowerBlue]
+      APHIS
+      
+      node [shape = box
+      fillcolor = Plum]
+      PoPSS
+      TL
+      'Port Targeting'
+      
+      node [shape = box
+      fillcolor = DarkSeaGreen]
+      'SOD Farm Bill'
+      'Weather Coeff Creation'
+      'Host Map Library'
+      'Parameter Estimation'
+      'Pest/Pathogen Parameter Database'
+      'Citrus Greening (HLB)'
+      'SLF Demo'
+
+      node [shape = box
+      fillcolor = MediumSlateBlue]
+      'Interactive Interface'
+      'Visualizations'
+
+      # add edge statements
+      APHIS -> PoPSS
+      APHIS -> TL
+      APHIS -> 'Port Targeting'
+      PoPSS -> 'SOD Farm Bill'
+      PoPSS -> 'Weather Coeff Creation'
+      PoPSS -> 'Host Map Library'
+      PoPSS -> 'Parameter Estimation'
+      PoPSS -> 'Pest/Pathogen Parameter Database'
+      PoPSS -> 'Citrus Greening (HLB)'
+      TL -> 'Citrus Greening (HLB)'
+      TL -> 'SLF Demo'
+
+
+      PoPSS -> 'Interactive Interface' [arrowhead = none]
+      PoPSS -> 'Visualizations' [arrowhead = none]
+      TL -> 'Visualizations' [arrowhead = none]
+      TL -> 'Interactive Interface' [arrowhead = none]
+
+      
+      # Add a graph statement
+      graph [nodestep =0.3, overlap = true, fontsize = 16, layout = circo]
+      }
+      
+      ")
--- org_chart.R
+++ org_chart.R
@@ -5,48 +5,96 @@ grViz("
 digraph boxes_and_circles {
 
 # add node statements
-node [shape = diamond
+node [shape = box
       fontname = Arial
       perwidth = 4.0
       style = filled
       fontsize = 16
       fillcolor = CornflowerBlue]
-APHIS
+'Ross Meentemeyer
+Director CGA'
 
 node [shape = box
       fillcolor = Plum]
-PoPSS
-TL
-'Port Targeting'
+'Chris Jones
+Lead Project Developer'
+  
+'Payam Tabriz
+UI/UX Designer'
+  
+'Vashek Petras
+Developer (Neural Networks)'
+  
+'Anna Petasova
+Developer (Spatial Steering'
+  
+'Megan Skrip
+Science Communication'
+  
+'Kunwar Singh
+Developer (RS/ML)'
 
-node [shape = circle
+node [shape = box
       fillcolor = DarkSeaGreen]
-'SOD Farm Bill'
-'Weather Coeff Creation'
-'Host Map Library'
-'Parameter Estimation'
-'Pest/Pathogen Parameter Database'
-'Citrus Greening (HLB)'
-'SLF Demo'
+'Devon Gaydos
+Stakeholder Engagement'
+  
+'Garret Miller
+User experience'
+  
+'Nick Kruskamp
+Developer (RS/ML)'
+
+'Kellyn Montgomery
+UAV integration'
+  
+'Umesh
+Developer (Adaptive mesh)'
+
 # add edge statements
-APHIS -> PoPSS
-APHIS -> TL
-APHIS -> 'Port Targeting'
-PoPSS -> 'SOD Farm Bill'
-PoPSS -> 'Weather Coeff Creation'
-PoPSS -> 'Host Map Library'
-PoPSS -> 'Parameter Estimation'
-PoPSS -> 'Pest/Pathogen Parameter Database'
-PoPSS -> 'Citrus Greening (HLB)'
-PoPSS -> 'Interactive Interface'
-PoPSS -> 'Visualizations'
-TL -> 'Visualizations'
-TL -> 'Interactive Interface'
-TL -> 'Citrus Greening (HLB)'
-TL -> 'SLF Demo'
+'Ross Meentemeyer
+Director CGA' -> 'Chris Jones
+Lead Project Developer';
+'Chris Jones
+Lead Project Developer' -> {
+'Payam Tabriz
+UI/UX Designer'
+'Vashek Petras
+Developer (Neural Networks)'
+'Anna Petasova
+Developer (Spatial Steering'
+      
+'Megan Skrip
+Science Communication'
+      
+'Kunwar Singh
+Developer (RS/ML)'
+}
+
+'Kunwar Singh
+Developer (RS/ML)' ->'Nick Kruskamp
+Developer (RS/ML)'
+
+'Vashek Petras
+Developer (Neural Networks)' -> {
+'Kellyn Montgomery
+UAV integration'
+'Umesh
+Developer (Adaptive mesh)'
+}
+
+'Payam Tabriz
+UI/UX Designer' ->
+'Garret Miller
+User experience'
+
+'Megan Skrip
+Science Communication' ->
+'Devon Gaydos
+Stakeholder Engagement'
 
 # Add a graph statement
-graph [nodestep =0.3, overlap = true, fontsize = 16, layout = circo]
+graph [nodestep =0.3, overlap = true, fontsize = 16, layout = dot]
       }
       
 ")
