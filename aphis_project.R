c749473900df8a0c25f5e02e459d71492356980d
Chris Jones <cjones1688@gmail.com>
2018-06-20 13:08:55 -0400

    update

--- aphis_project.R
+++ aphis_project.R
@@ -11,13 +11,11 @@ grViz("
       style = filled
       fontsize = 16
       fillcolor = CornflowerBlue]
-      APHIS
       
       node [shape = box
       fillcolor = Plum]
       PoPSS
       TL
-      'Port Targeting'
       
       node [shape = box
       fillcolor = DarkSeaGreen]
@@ -25,37 +23,32 @@ grViz("
       'Weather Coeff Creation'
       'Host Map Library'
       'Parameter Estimation'
-      'Pest/Pathogen Parameter Database'
       'Citrus Greening (HLB)'
-      'SLF Demo'
+      'SLF'
+      'Pest/Pathogen Parameter Database'
+      'Web'
 
       node [shape = box
       fillcolor = LightSalmon]
-      'Interactive Interface'
-      'Visualizations'
+      'Visualizations/Interface'
+      'Case Studies'
 
       # add edge statements
-      APHIS -> PoPSS
-      APHIS -> TL
-      APHIS -> 'Port Targeting'
-      PoPSS -> 'SOD Farm Bill'
+      'Case Studies' -> 'SOD Farm Bill'
       PoPSS -> 'Weather Coeff Creation'
       PoPSS -> 'Host Map Library'
       PoPSS -> 'Parameter Estimation'
-      PoPSS -> 'Pest/Pathogen Parameter Database'
-      PoPSS -> 'Citrus Greening (HLB)'
-      TL -> 'Citrus Greening (HLB)'
-      TL -> 'SLF Demo'
+      'Case Studies' -> 'Citrus Greening (HLB)'
+      'Case Studies' -> 'SLF'
 
+      PoPSS -> 'Case Studies'
+      'Visualizations/Interface' -> 'Pest/Pathogen Parameter Database' [arrowhead = none] 
+      PoPSS -> 'Visualizations/Interface' [arrowhead = none]
+      TL -> 'Visualizations/Interface' [arrowhead = none]
+      'Visualizations/Interface' -> 'Web' [arrowhead = none]
 
-      PoPSS -> 'Interactive Interface' [arrowhead = none]
-      PoPSS -> 'Visualizations' [arrowhead = none]
-      TL -> 'Visualizations' [arrowhead = none]
-      TL -> 'Interactive Interface' [arrowhead = none]
-
-      
       # Add a graph statement
       graph [nodestep =0.3, overlap = true, fontsize = 16, layout = circo]
       }
-      
+
       ")
